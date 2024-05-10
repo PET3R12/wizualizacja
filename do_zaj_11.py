9acc062029c8addf6d9f01bc687afdc085e421da
PET3R12 <147385535+PET3R12@users.noreply.github.com>
2024-05-10 10:11:13 +0200

    Create do_zaj_11.py

---
+++ do_zaj_11.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+f = open('penguins.csv', 'r+')
+s = f.readlines()
+usecols = ['species', 'island', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex']
+
+data = pd.read_csv("penguins.csv", delimiter=None, engine='python')
+df = pd.DataFrame(data, columns=usecols, )
+suma = 0
+#średnia waga w każdej płci, średnia waga z podziałem na płeć i gatunek
+
+df['body_mass_g'] = df['body_mass_g'].fillna(0).astype(int)
+for element in df['body_mass_g']:
+    element = int(element)
+suma = sum(df['body_mass_g'][df['sex'] == 'MALE'])
+ilosc = len(df['body_mass_g'][df['sex'] == 'MALE'])
+print(f'MALE: {suma/ilosc}')
+suma = sum(df['body_mass_g'][df['sex'] == 'FEMALE'])
+ilosc = len(df['body_mass_g'][df['sex'] == 'FEMALE'])
+print(f'FEMALE: {suma/ilosc}')
+print(data.groupby('sex'))
