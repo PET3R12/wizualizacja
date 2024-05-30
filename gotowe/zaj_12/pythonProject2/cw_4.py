ba88cf1b38ce4509c44cbd955dbcec09351fe097
PET3R12 <p.ostaszewski@o2.pl>
2024-05-30 19:04:59 +0200

    zmiany2

---
+++ gotowe/zaj_12/pythonProject2/cw_4.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.ticker as mtick
+
+glue = sns.load_dataset('glue')
+data = glue['Score'][glue['Model'] == 'ERNIE']
+year = [2017, 2018, 2019]
+
+#create catplot
+glue_df = sns.catplot(
+    data=glue,
+    kind='violin',
+    x='Year',
+    y='Score',
+    hue='Encoder',
+    col = 'Model',
+    palette='Set2'
+)
+#set labels
+glue_df.set_axis_labels('Tasks', 'Scores')
+glue_df
+plt.show()
